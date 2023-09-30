464d1e2b5ddfbc9fff7e1733d9a3a5e9ab4b2cff
Dylan <dylan_krajnc@tutanota.com>
2023-09-30 07:02:59 +0100

    Nth Fibonacci Question
    - added a solution to the nth fibonacci question, which makes use of recursion

---
+++ DSA Questions in C#/Nth Fibonacci - Recursion/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        static int GetNthFib(int n)
+        {
+            if (n == 0 || n == 1)
+                return 0;
+            if (n == 2)
+                return 1;
+            else
+                return GetNthFib(n - 1) + GetNthFib(n - 2);
+        }
+
+        Console.WriteLine($"The 6th number in the fibonacci sequence is: {GetNthFib(6)}");
+    }
+}
