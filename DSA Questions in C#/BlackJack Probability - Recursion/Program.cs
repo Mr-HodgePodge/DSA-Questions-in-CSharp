b6b8221509d8fc56e22074ec77fad21f8c01fdd3
Dylan <dylan_krajnc@tutanota.com>
2023-10-01 04:23:10 +0100

    BlackJack Probability Question
    - answer to the blackjack probability question by making use of recursion and memoization

---
+++ DSA Questions in C#/BlackJack Probability - Recursion/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // Algorithm question using recursion and memoization to find the probability of a dealer hand busting in blackjack.
+        // The best solution is the function BlackjackProbability (with helper function), with a time complexity of O(t - s) and a space complexity of O(t - s)
+
+        static float BlackjackProbability(float target, float startingHand)
+        {
+            var dict = new Dictionary<float, float>();
+
+            return (float)Math.Round(calculateProbability(target, startingHand, dict), 3);
+        }
+
+        static float calculateProbability(float target, float currentHand, Dictionary<float, float> dict)
+        {
+            if (dict.ContainsKey(currentHand))
+                return dict[currentHand];
+            if (currentHand > target)
+                return 1;
+            if (currentHand + 4 >= target)
+                return 0;
+
+            float probability = 0f; 
+            for (int i = 1; i < 11; i++)
+                probability += 0.1f * calculateProbability(target, currentHand + i, dict);
+            dict[currentHand] = probability;
+
+            return probability;
+        }
+
+        float[] arr = { 15f };
+
+        foreach (float hand in arr) 
+            Console.WriteLine($"The probability of a dealer hand {hand} busting is: {BlackjackProbability(21f, hand)}");
+    }
+}
